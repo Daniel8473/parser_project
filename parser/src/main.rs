06a77fede1109789c851f5800fcdad603a91cbbf
Soumya Duriseti <soumyaduriseti@gmail.com>
2022-05-26 01:13:19 -0700

    stage  2

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,15 +1,26 @@
 use std::env;
 use std::fs;
 
+extern crate regex;
+use regex::Regex;
+
 fn main() {
     // stage 3.1
     // read in file from command line
     let args: Vec<String> = env::args().collect();
     let file_name = args[1].clone();
-    // create CStream object to place file into vec
-    let f: CStream = CStream::new(&file_name);
+    // create CStream object to place file into vec (separated by \n)
+    let stream: CStream = CStream::new(&file_name);
+    // tokenize vec
+    for 
+    let all_tokens: Vec<Token> = scanner(stream.f_vec);
     // print to screen
-    f.print_vec();
+    println!("{:?}", stream.f_str);
+    for t in all_tokens.iter() {
+        println!("{}", t.token_type.as_str());
+    }
+    //println!("{:?}", all_tokens);
+
 
     // stage 3.2
 }
@@ -24,22 +35,26 @@ enum TokenType {
     WHITESPACE
 }
 
-// impl TokenType{
-//     fn as_str (&self) -> &'static str {
-//         // formats the type as string to help with printing
-//         match &self{
-//             TokenType::CONSTANT => "constant",
-//             TokenType::OPERATOR => "operator",
-//             TokenType::VARIABLE => "variable",
-//             TokenType::SPECIAL => "special symbol"
-//         }
+impl TokenType{
+    
+    fn as_str (&self) -> &'static str {
+        // formats the type as string to help with printing
+        match &self{
+            TokenType::INT_CONSTANT => "INT_CONSTANT",
+            TokenType::FLOAT_CONSTANT => "FLOAT_CONSTANT",
+            TokenType::KEYWORD => "KEYWORD",
+            TokenType::OPERATOR => "OPERATOR",
+            TokenType::WHITESPACE => "WHITESPACE",
+            TokenType::INVALID => "INVALID",
+            TokenType::IDENTIFIER => "IDENTIFIER"
+        }
 
-//     }
-// }
+    }
+}
 
 struct Token {
     text: String,
-    token_type: TokenType
+    token_type: TokenType,
     line_num: i32,
     char_pos: i32
 }
@@ -50,27 +65,29 @@ impl Token {
     let mut typ = TokenType::INVALID;
     //[[:alpha:]]
     //[[:digit:]]
-//     x*        zero or more of x (greedy)
-// x+        one or more of x (greedy)
-// x?        zero or one of x (greedy)
-// xy    concatenation (x followed by y)
-// x|y   alternation (x or y, prefer x)
+    //     x*        zero or more of x (greedy)
+    // x+        one or more of x (greedy)
+    // x?        zero or one of x (greedy)
+    // xy    concatenation (x followed by y)
+    // x|y   alternation (x or y, prefer x)
     
     // assigns to appropriate type upon creation
 
     match t {
-        // IDENTIFIER: ( _ | \d ) ( _ | \d | Alpha )*
-        //(_|[A-Za-z])([_|\d|[A-Za-z]])*
-        //INT_CONSTANT [-]?\d+
-        //FIXME [-]?\d+[.[\d]+]?
+        // IDENTIFIER: (_|[A-Za-z])([_|\d|[A-Za-z]])*
+        // INT_CONSTANT: [-]?\d+
+        // FLOAT_CONSTANT: [-]?\d+[.\d+]?
         "unsigned" | "char" | "short" | "int" | "long" | "float" | "double" | "while" | "if" | "return" | "void" | "main" => typ = TokenType::KEYWORD,
-        "(" | "," | ")" | "{" | "}" | "=" | "==" | "<" | ">" | ">=" | ">=" | "!=" | "+" | "-" | "*" | "/" | ";" => typ = TokenType::OPERATOR,
-        t.chars().nth(0).unwrap().is_whitespace() => typ = TokenType::WHITESPACE,
-        => typ = TokenType::INT_CONSTANT,
-        => typ = TokenType::FLOAT_CONSTANT,
-        => typ = TokenType::IDENTIFIER,
+        "(" | "," | ")" | "{" | "}" | "=" | "==" | "<" | ">" | ">=" | "<=" | "!=" | "+" | "-" | "*" | "/" | ";" => typ = TokenType::OPERATOR,
         _ => typ = TokenType::INVALID
     }
+    if typ.as_str() == "INVALID" {
+        if Regex::new(r"\s").unwrap().is_match(t) {typ = TokenType::WHITESPACE;}
+        else if Regex::new(r"[-]?\d+").unwrap().is_match(t) {typ = TokenType::INT_CONSTANT;}
+        else if Regex::new(r"[-]?\d+[.\d+]?").unwrap().is_match(t){ typ = TokenType::FLOAT_CONSTANT;}
+        else if Regex::new(r"(_|[A-Za-z])([_|\d|[A-Za-z]])*").unwrap().is_match(t) {typ = TokenType::IDENTIFIER;}
+    }
+    println!("the {} token was assigned type {}", t, typ.as_str());
 
         Token {
             text: t.to_string(),
@@ -82,19 +99,25 @@ impl Token {
     }
 }
 
-struct Scanner {
-    // parse file, calls token for each token it identifies
-}
 
-impl Scanner {
-    fn new() -> Scanner {
-        Scanner{}
-        parse_input(){
 
-            //look at string
-            //make token "0", ":="
+
+fn scanner(f_vec: Vec<String>) -> Vec<Token> {
+    // loop through file and tokenize
+    let mut token_vec: Vec<Token> = vec![];
+    for line in f_vec.iter() {
+        //TODO
+        for c in line {
+            // soumyas hw
+
+            // ...
+            //let t: Token = Token::new(________);
+            //token_vec.push(__________);
         }
+        
+        
     }
+    return token_vec;
 }
 
 struct CStream {
@@ -108,14 +131,11 @@ impl CStream {
         let f_s = fs::read_to_string(file_name).unwrap();
         // splits string at newlines and places substring into vector
         let mut f_v: Vec<String> = f_s.lines().map(String::from).collect();
-
+        
         CStream{
             f_vec: f_v,
             f_str: f_s
         }
     }
-
-    fn print_vec(&self) {
-        println!{"{:?}", self.f_str};
-    }
 }
+
